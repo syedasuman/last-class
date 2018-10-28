43b5001381373de12df09a9805f26b466c85fabc
syeda suman <38942910+syedasuman@users.noreply.github.com>
2018-10-28 12:52:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// function checkPrime(){
+// 	let inp =parseInt(document.querySelector('input').value);
+// 	let i;
+// 	for (i = 2; i < inp; i++) {
+// 		if (inp % i == 0){
+// 			break;
+// 		}
+// 	}
+// 	if(i == inp){
+// 		alert("it is a prime number");
+// 	}else{
+// 		alert ("it is not a prime number");
+// 	}
+// }
+// function isPrime(numb){
+// 	let i;
+// 	for (i = 2; i < numb; i++) {
+// 		if (numb % i == 0){
+//  			break;
+//  		}
+//  	}
+//  	if(i === numb){
+// 		return true;
+//  	}else{
+//  		return false;
+//  	}
+// }
+// 	for ( let i = 2; i <= 100; i++) {
+// 		if (isPrime(i)){
+// 		console.log(i);
+			
+// 		} 
+// 	}
